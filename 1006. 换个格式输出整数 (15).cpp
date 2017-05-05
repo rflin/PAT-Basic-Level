e28e020bce8e83cf2e1dbeb28a153efad667214c
GuoLin <462650965@qq.com>
2017-05-05 23:37:17 +0800

    Create 1006. 换个格式输出整数 (15).cpp

---
+++ 1006. 换个格式输出整数 (15).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n,Bn,Sn,Gn;
+    cin>>n;
+    Bn=n/100;
+    Sn=n%100/10;
+    Gn=n%10;
+    while(Bn--)cout<<"B";
+    while(Sn--)cout<<"S";
+    for(int i=1;i<=Gn;++i)cout<<i;
+    return 0;
+}
