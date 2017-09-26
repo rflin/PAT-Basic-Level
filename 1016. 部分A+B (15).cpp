fefe5acf39ca751faca49b5052b9d61be174b427
L <462650965@qq.com>
2017-09-27 00:39:28 +0800

    -

---
+++ 1016. 部分A+B (15).cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+using namespace std;
+long long part(string &n,char d)
+{
+    string str;
+    for(auto &x:n)
+        if(x==d) str+=d;
+    return str.size()?stoll(str):0;
+}
+int main()
+{
+    string a,b;
+    char da,db;
+    cin>>a>>da>>b>>db;
+    cout<<part(a,da)+part(b,db);
+    return 0;
+}
