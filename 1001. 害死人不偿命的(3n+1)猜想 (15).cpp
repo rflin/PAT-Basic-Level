e39e3b9b457fb5c15ef1306b24781a82c9ab5e65
L <462650965@qq.com>
2018-01-10 11:16:03 +0800

    -

--- 1001. 害死人不偿命的(3n+1)猜想 (15).cpp
+++ 1001. 害死人不偿命的(3n+1)猜想 (15).cpp
@@ -1,5 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
+int main(int argc, char const *argv[])
+{
+    int n,count=0;
+    cin>>n;
+    while(n!=1)
+    {
+        n=(n%2==1)?(3*n+1)/2:n/2;
+        ++count;
+    }
+    cout<<count;
+    return 0;
+}
+
+/*以下为递归求解版本
+#include <bits/stdc++.h>
+using namespace std;
 int solve(int n)
 {
     if(n==1) return 0;
@@ -12,3 +28,4 @@ int main(int argc, char const *argv[])
     cout<<solve(n);
     return 0;
 }
+*/
